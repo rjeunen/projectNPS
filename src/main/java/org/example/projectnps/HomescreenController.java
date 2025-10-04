3deec192db4fa02e04cddc4c7f72f40fe139d8f4
Robby Jeunen <robbyjeunen@gmail.com>
2025-10-04 20:40:52 +0200

    added getData functions and onaction button import - method is not complete

--- src/main/java/org/example/projectnps/HomescreenController.java
+++ src/main/java/org/example/projectnps/HomescreenController.java
@@ -6,6 +6,11 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class HomescreenController {
     @FXML
     private AnchorPane homeScreenPane;
@@ -33,4 +38,41 @@ public class HomescreenController {
 
     @FXML
     private Button exportButton;
+
+    @FXML
+    protected void getData(){
+        try{
+            //onderstaande moet nog verwijzen naar nps.csv MAAR eerst navragen of dit wel klopt?
+            BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\robje\\projectNpsCsvData\\test.csv"));
+
+            /*
+            * Deze while loop gaat elke lijn in een array duwen.
+            * Zou ik nu ook een 2D array moeten maken die vb [1000] 1000 lijnen kan bevatten en [aantal kolommen] (afhankelijk van CSV) moet bevatten
+            * Dan in diezelfde while loop kan ik de data direct op de juiste plaats weergeven?
+            * check JAVAFX PPT - DIA 27
+            * Ik weet niet of de devices[][] klopt hieronder EN heb een vaste grootte van [1000][8] gemaakt wat ook niet goed is vermoed ik
+            * */
+            String line;
+            String[][] devices = new String[1000][8];
+            int deviceCounter = 0;
+            while((line = br.readLine()) != null){
+                String [] csvValues = line.split(";"); // regex nakijken in de CSV - moet dit op spatie?
+                devices[deviceCounter][0] = csvValues[0];
+                devices[deviceCounter][1] = csvValues[1];
+                devices[deviceCounter][2] = csvValues[2];
+                devices[deviceCounter][3] = csvValues[3];
+                devices[deviceCounter][4] = csvValues[4];
+                devices[deviceCounter][5] = csvValues[5];
+                devices[deviceCounter][6] = csvValues[6];
+                devices[deviceCounter][7] = csvValues[7];
+                deviceCounter++;
+            }
+            //System.out.println(Arrays.toString(csvValues));
+            System.out.println(Arrays.deepToString(devices));
+            //System.out.println(devices[1][1]);
+        }
+        catch (IOException e){
+            System.out.println("Something went wrong retrieving the CSV file");
+        }
+    }
 }
