728a0f3a464718e8675a70f929ae1d323683c407
Robby Jeunen <robbyjeunen@gmail.com>
2025-10-04 21:41:08 +0200

    litte rework on getData using ArrayList instead of Array

--- src/main/java/org/example/projectnps/HomescreenController.java
+++ src/main/java/org/example/projectnps/HomescreenController.java
@@ -9,7 +9,9 @@ import javafx.scene.layout.VBox;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class HomescreenController {
     @FXML
@@ -41,35 +43,26 @@ public class HomescreenController {
 
     @FXML
     protected void getData(){
-        try{
-            //onderstaande moet nog verwijzen naar nps.csv MAAR eerst navragen of dit wel klopt?
-            BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\robje\\projectNpsCsvData\\test.csv"));
+        /*
+         *Vraag na of de CSV wel juist is - geen juiste file?!
+         * Deze method heeft nog werk nodig!
+         * Volgende stap is om de data zichtbaar te krijgen in de view - tableView?
+         * */
 
-            /*
-            * Deze while loop gaat elke lijn in een array duwen.
-            * Zou ik nu ook een 2D array moeten maken die vb [1000] 1000 lijnen kan bevatten en [aantal kolommen] (afhankelijk van CSV) moet bevatten
-            * Dan in diezelfde while loop kan ik de data direct op de juiste plaats weergeven?
-            * check JAVAFX PPT - DIA 27
-            * Ik weet niet of de devices[][] klopt hieronder EN heb een vaste grootte van [1000][8] gemaakt wat ook niet goed is vermoed ik
-            * */
+        List<String[]> csvValues = new ArrayList<>();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\robje\\projectNpsCsvData\\nps.csv"));
+
             String line;
-            String[][] devices = new String[1000][8];
-            int deviceCounter = 0;
             while((line = br.readLine()) != null){
-                String [] csvValues = line.split(";"); // regex nakijken in de CSV - moet dit op spatie?
-                devices[deviceCounter][0] = csvValues[0];
-                devices[deviceCounter][1] = csvValues[1];
-                devices[deviceCounter][2] = csvValues[2];
-                devices[deviceCounter][3] = csvValues[3];
-                devices[deviceCounter][4] = csvValues[4];
-                devices[deviceCounter][5] = csvValues[5];
-                devices[deviceCounter][6] = csvValues[6];
-                devices[deviceCounter][7] = csvValues[7];
-                deviceCounter++;
+                String[] lineValues = line.split(" "); //regex is spatie
+                csvValues.add(lineValues);
             }
-            //System.out.println(Arrays.toString(csvValues));
-            System.out.println(Arrays.deepToString(devices));
-            //System.out.println(devices[1][1]);
+            //data van 1 rij naar een array duwen - 1 volledige rij is dus [0] ... - betere oplossing voor?
+            for(int i = 0; i < csvValues.size(); i++){
+                String[] row = csvValues.get(i);
+                System.out.println("Rij " + i + " :" + Arrays.toString(row));
+            }
         }
         catch (IOException e){
             System.out.println("Something went wrong retrieving the CSV file");
