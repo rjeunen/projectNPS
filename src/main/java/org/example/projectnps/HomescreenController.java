263e8c23697c4e563898df59eead1b195f960ac1
Robby Jeunen <robbyjeunen@gmail.com>
2025-10-04 19:33:32 +0200

    added initial fxml and anchorpanes - labels - buttons

---
+++ src/main/java/org/example/projectnps/HomescreenController.java
@@ -0,0 +1,36 @@
+package org.example.projectnps;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class HomescreenController {
+    @FXML
+    private AnchorPane homeScreenPane;
+
+    @FXML
+    private AnchorPane creditsPane;
+
+    @FXML
+    private AnchorPane navigationPane;
+
+    @FXML
+    private AnchorPane contentPane;
+
+    @FXML
+    private Label creditsLabel;
+
+    @FXML
+    private Label titleLabel;
+
+    @FXML
+    private VBox containerVbox;
+
+    @FXML
+    private Button importButton;
+
+    @FXML
+    private Button exportButton;
+}
