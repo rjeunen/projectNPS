d9abe1e29f65920b5a07104a2703afa4dbb66349
Robby Jeunen <robbyjeunen@gmail.com>
2025-10-18 17:47:11 +0200

    renamed interface - start with I

--- src/main/java/org/example/projectnps/IDataImporter.java
+++ src/main/java/org/example/projectnps/IDataImporter.java
@@ -2,6 +2,6 @@ package org.example.projectnps;
 
 import java.util.List;
 
-public interface DataImporter {
+public interface IDataImporter {
     List<DataRecord> readFromFile (String filePath);
 }
