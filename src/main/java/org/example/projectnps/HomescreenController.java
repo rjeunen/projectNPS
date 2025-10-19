885c7a18cc820f6235ab20f0e427cd5f5d85e676
Robby Jeunen <robbyjeunen@gmail.com>
2025-10-19 19:34:33 +0200

    de Tableview is bewerkbaar - kilom processingORder kan bewerkt worden = nieuze waarde wordt bewaart

--- src/main/java/org/example/projectnps/HomescreenController.java
+++ src/main/java/org/example/projectnps/HomescreenController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
@@ -69,6 +70,9 @@ public class HomescreenController {
 
     @FXML
     public void initialize(){
+        //aangeven dat de gebuiker wijwigingen mag maken
+        contentTableView.setEditable(true);
+
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         stateColumn.setCellValueFactory(new PropertyValueFactory<>("state"));
         processingOrderColumn.setCellValueFactory(new PropertyValueFactory<>("processingOrder"));
@@ -77,6 +81,14 @@ public class HomescreenController {
         conditionDataColumn.setCellValueFactory(new PropertyValueFactory<>("conditionData"));
         profileIdColumn.setCellValueFactory(new PropertyValueFactory<>("profileId"));
         profileDataColumn.setCellValueFactory(new PropertyValueFactory<>("profileData"));
+
+        //processingOrder kolom effectief bewerkbaar makent
+        //TextFieldTableCell.forTableColumn() betekent, gebrui een textfield bij aanpassingen
+        processingOrderColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        processingOrderColumn.setOnEditCommit(event -> {
+            DataRecord record = event.getRowValue();
+            record.setProcessingOrder(event.getNewValue());
+        });
     }
 
     @FXML
