7e36dd4ac11f345467ade21a0c4f821d7440cc22
Robby Jeunen <robbyjeunen@gmail.com>
2025-10-29 20:07:00 +0100

    added addREcord FXML and AddREcordController

---
+++ src/main/java/org/example/projectnps/AddRecordController.java
@@ -0,0 +1,36 @@
+package org.example.projectnps;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AddRecordController {
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField stateField;
+    @FXML
+    private TextField processingOrderField;
+    @FXML
+    private TextField policySourceField;
+    @FXML
+    private TextField conditionIdField;
+    @FXML
+    private TextField conditionDataField;
+    @FXML
+    private TextField profileIdField;
+    @FXML
+    private TextField profileDataField;
+
+    public DataRecord getRecord() {
+        return new DataRecord(
+                nameField.getText(),
+                stateField.getText(),
+                processingOrderField.getText(),
+                policySourceField.getText(),
+                conditionIdField.getText(),
+                conditionDataField.getText(),
+                profileIdField.getText(),
+                profileDataField.getText()
+        );
+    }
+}
