1fbca079ec52dfc566dbf7723be66dcafb794166
Robby Jeunen <robbyjeunen@gmail.com>
2025-11-02 12:28:10 +0100

    added logo of atlas college

--- src/main/java/org/example/projectnps/HelloApplication.java
+++ src/main/java/org/example/projectnps/HelloApplication.java
@@ -3,6 +3,7 @@ package org.example.projectnps;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("homeScreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        Image atlasLogo = new Image("atlasLogo.jpg");
+        stage.getIcons().add(atlasLogo);
         stage.setTitle("project NPS");
         stage.setScene(scene);
         stage.show();
